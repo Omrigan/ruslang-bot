8235e1d579f74e0e510f88460f2e22dab1b5cb8c
Omrigan <omrigann@gmail.com>
2016-03-24 16:27:12 +0300

    Help

--- secret_settings.py
+++ secret_settings.py
@@ -4,9 +4,8 @@ if env=='debug':
     bot = {'token' : '183365432:AAEs5OxDxYAZ22qJqjM4SzoNtv-GmyTKOsQ'}
     mongo = {'uri': 'localhost'}
 else:
-    pass
-bot = {'token': '157704339:AAH9HsU_e90F8xosACaZJC4NPviGR0Z2IYo'}
-mongo = {'uri': 'mongodb://root:monr9900@equal.cf'}
+    bot = {'token': '157704339:AAH9HsU_e90F8xosACaZJC4NPviGR0Z2IYo'}
+    mongo = {'uri': 'mongodb://root:monr9900@equal.cf'}
 
 
 
